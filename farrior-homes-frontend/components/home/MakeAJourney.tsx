772a0789934124289fbaaa5723d0919d760f07a5
rakib3719 <bannah76769@gmail.com>
2026-04-25 20:25:45 +0600

    feat: make a journey

--- farrior-homes-frontend/components/home/MakeAJourney.tsx
+++ farrior-homes-frontend/components/home/MakeAJourney.tsx
@@ -6,7 +6,7 @@ export default function MakeAJourney() {
   return (
     <div className='md:mx-12.5'>
       <div className='grid grid-cols-1 md:grid-cols-12 gap-6 my-16 md:my-20 px-4 md:px-8 '>
-        <div className='md:col-span-4 bg-[#304C3E] text-white px-8 py-14 md:py-26 rounded-lg md:rounded-r-none'>
+        <div className='md:col-span-4 bg-[#03404e] text-white px-8 py-14 md:py-26 rounded-lg md:rounded-r-none'>
           <Title
             title={"Your South Suburbs Dream Home Awaits"}
             subtitle={
